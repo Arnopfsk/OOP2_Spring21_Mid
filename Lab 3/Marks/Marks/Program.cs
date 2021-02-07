d61496cb2ecfc22b646170deea43044a6d87f9c8
19-39906-1@student.aiub.edu <19-39906-1@student.aiub.edu>
2021-02-07 12:35:01 +0600

    lab3

---
+++ Lab 3/Marks/Marks/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double phy = 50.5, che = 60.6, bio = 70.7, math = 80.8, com = 90.9;
+            double sum = phy + che + bio + math + com;
+            sum /= 5.0;
+            Console.WriteLine("per" + sum + "%");
+            if (sum < 50)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (sum >= 50 && sum < 75)
+            {
+                Console.WriteLine("C+");
+            }
+            else if (sum >= 75 && sum <= 79)
+            {
+                Console.WriteLine("B");
+            }
+            else if (sum >= 80 && sum < 85)
+            {
+                Console.WriteLine("B+");
+            }
+            else if (sum >= 85 && sum < 89)
+            {
+                Console.WriteLine("A");
+            }
+            else
+            {
+                Console.WriteLine("A+");
+            }
+        }
+    }
+}
---
+++ Lab 3/Sumoddeven/Sumoddeven/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sumoddeven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int odd = 0, even = 0;
+            for (int i = 1; i <= 49; i += 2)
+            {
+                odd += i;
+            }
+            for (int i = 2; i <= 50; i += 2)
+            {
+                even += i;
+            }
+            Console.WriteLine("Sum of Even Numbers : " + even);
+            Console.WriteLine("Sum of odd Numbers : " + odd);
+        }
+    }
+}
