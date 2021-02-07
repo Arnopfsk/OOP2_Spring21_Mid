28d236b2c3caaafaa282f9ae42fefff5687f8f49
19-39906-1@student.aiub.edu <19-39906-1@student.aiub.edu>
2021-02-07 13:58:45 +0600

    lab3

---
+++ Lab 3/Classdesigne/Classdesigne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classdesigne
+{
+    public class Program
+    {
+        string name;
+        string id;
+        string department;
+        float cgpa;
+    }
+}
