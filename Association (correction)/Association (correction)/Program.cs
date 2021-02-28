eb89df932a7b872f397802363a825073f8a0e6fb
19-39906-1@student.aiub.edu <19-39906-1@student.aiub.edu>
2021-02-28 15:46:15 +0600

    Association (Correction)

---
+++ Association (correction)/Association (correction)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association__correction_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department d1 = new Department("Computer Science", "CS");
+            Student s1 = new Student("ARNOP", "19-39906-1", 3.63f);
+            d1.AllStudents();
+            s1.Department = d1;
+            s1.ShowInfo();
+
+        }
+    }
+}
---
+++ Association (correction)/Association (correction)/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association__correction_
+{
+    class Student
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private string id;
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private float cg;
+        public float CG
+        {
+            get { return cg; }
+            set { cg = value; }
+        }
+        Department department;
+        public Department Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+        public Student() { }
+        public Student(string name, string id, float cg)
+        {
+            this.name = name;
+            this.id = id;
+            this.cg = cg;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Id:" + id);
+            Console.WriteLine("CG:" + CG);
+        }
+    }
+}
