aa8abcb7bf1b9a943f3a46a7f5115564b08b5463
19-39906-1@student.aiub.edu <19-39906-1@student.aiub.edu>
2021-02-28 14:39:45 +0600

    association

---
+++ association/association/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assosiation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Depertment d1 = new Depertment("Computer Science", "CSE");
+            Depertment d2 = new Depertment("Business Admin.", "BBA");
+            Student s1 = new Student("ARNOP", "555", 2.5f);
+            Student s2 = new Student("FAHIM", "666", 5.35f);
+            s1.Depertment = d1;
+            s2.Depertment = d2;
+            s1.show();
+            s2.show();
+
+
+        }
+    }
+}
---
+++ association/association/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assosiation
+{
+    class Student
+    {
+        private string name;
+        private string Name
+        {
+            set { name = value; }
+            get { return name; }
+
+        }
+        private string id;
+        private string ID
+        {
+            set { id = value; }
+            get { return id; }
+
+        }
+        private float cg;
+        private float CG
+        {
+            set { cg = value; }
+            get { return cg; }
+
+        }
+        public Student() { }
+        public Student(string name, string id, float cg)
+        {
+            this.name = name;
+            this.id = id;
+            this.cg = cg;
+        }
+        Depertment depertment;
+        public Depertment Depertment
+        {
+            set { depertment = value; }
+            get { return depertment; }
+        }
+        public void show()
+        {
+            Console.WriteLine("Name : " + name);
+            Console.WriteLine("ID : " + id);
+            Console.WriteLine("CG : " + cg);
+        }
+    }
+}
